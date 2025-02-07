4a38feae8f837cb21a68179968fc4f38d71c17c8
OxyToan <toan@oxytoan.xyz>
2025-02-07 16:47:11 -0500

    Update nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/support',
+        destination: 'https://discord.gg/qBpWMHYdQx',
+        permanent: true,
+      },
+      {
+        source: '/invite/:id',
+        destination: 'https://discord.com/oauth2/authorize?client_id=1328111647440764928&permissions=572262987590870&integration_type=0&scope=applications.commands+bot&guild_id=:id',
+        permanent: true,
+      },
+      {
+        source: '/invite',
+        destination: 'https://discord.com/oauth2/authorize?client_id=1328111647440764928&permissions=572262987590870&integration_type=0&scope=applications.commands+bot',
+        permanent: true
+      },
+    ]
+  },
 };
 
 export default nextConfig;
